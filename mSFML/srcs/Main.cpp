2aead59832fd8294eb1f2f84748cd1d8785c47e6
billot_t <thomas.billot@epitech.eu>
2016-11-30 15:15:03 +0100

    Changes on main file

--- mSFML/srcs/Main.cpp
+++ mSFML/srcs/Main.cpp
@@ -1,18 +1,18 @@
 //
-// test.cpp for Gomuko in /home/mikaz3/Tek3/B5/AI
+// Main.cpp for Gomuko in /home/mikaz3/Tek3/B5/AI
 //
 // Made by Thomas Billot
 // Login   <mikaz3@epitech.net>
 //
 // Started on  Wed Oct 12 11:52:44 2016 Thomas Billot
-// Last update Wed Nov 30 15:02:30 2016 bogard_t
+// Last update Wed Nov 30 15:13:16 2016 Thomas Billot
 //
 
 # include		"Gomoku.hpp"
 
-int			main(int , char **)
+int			main(void)
 {
   Gomoku		gomoku;
-  gomoku.start();
-  return (0);
+  
+  return gomoku.start();
 }
