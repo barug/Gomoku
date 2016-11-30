0e7e0a5b683ad0cc31367c6b3b3990dc2b1e3c44
barthelemy gouby <barthelemy.gouby@gmail.com>
2016-11-30 15:12:33 +0100

    adding map files

---
+++ map.hpp
@@ -0,0 +1,34 @@
+#ifndef GOMOKU_MAP_HPP
+# define GOMOKU_MAP_HPP
+
+# include <vector>
+
+# define MAP_WIDTH	(19)
+# define MAP_SIZE	(MAP_WIDTH * MAP_WIDTH)
+
+class			Map
+{
+public:
+  enum			CaseState
+    {
+      EMPTY,
+      WHITE,
+      BLACK
+    };
+
+private:
+  std::vector<char>	_mapData;
+  
+public:
+  Map();
+  ~Map();
+
+  void				setCaseAt(const unsigned int &xxo,
+					  const unsigned int &y,
+					  const Map::CaseState &state);
+  const Map::CaseState		&getCaseAt(const unsigned int &x,
+					   const unsigned int &y);
+  const std::vector<char>	&getMapData();
+};
+
+#endif /* GOMOKU_MAP_HPP */
