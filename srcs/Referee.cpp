f9808276f6b07cde4eb1944969ad939f5ea98cce
bloy_j <josselin.bloy@epitech.eu>
2016-12-02 17:03:11 +0100

    testCapture

--- srcs/Referee.cpp
+++ srcs/Referee.cpp
@@ -10,7 +10,7 @@ IReferee::gameState	GomokuReferee::validatePlayerAction(const unsigned int &Coor
 							    const unsigned int &CoordY)
 {}
 
-int			countAlignement(const Map &map, int xInc, int yInc)
+int			countAlignement(const Map &map, Map::Coordinates coordinates, int xInc, int yInc)
 {
   int			count = 0;
 
@@ -41,8 +41,67 @@ int				testAlignement(GomokuReferee::Direction direction,
   return count;
 }
 
+bool				countCapture(const Map &map, Map::Coordinates coordinates, int xInc, int yInc)
+{
+  Map::CaseState rivals = map[MAP_WIDTH * coordinates.y + coordinates.x] == Map::CaseState::WHITE ? Map::CaseState::BLACK : Map::CaseState::WHITE;
+
+  if (map[MAP_WIDTH * (coordinates.y + yInc) + (coordinates.x + xInc)] == rivals &&
+      map[MAP_WIDTH * (coordinates.y + (yInc * 2)) + (coordinates.x + (xInc * 2))] == rivals &&
+      map[MAP_WIDTH * (coordinates.y + (yInc * 3)) + (coordinates.x + (xInc * 3))] == map[MAP_WIDTH * coordinates.y + coordinates.x])
+    return true;
+  return false;
+}
+
 bool				testCapture(GomokuReferee::Direction direction,
 					    const Map &map, Map::Coordinates coordinates)
 {
+  switch (direction)
+    {
+    case GomokuReferee::Direction::NORTH :
+      xInc = 0;
+      yInc = 1;
+      break;
 
+    case GomokuReferee::Direction::SOUTH :
+      xInc = 0;
+      yInc = -1;
+      break;
+
+    case GomokuReferee::Direction::WEST :
+      xInc = -1;
+      yInc = 0;
+      break;
+
+    case GomokuReferee::Direction::EAST :
+      xInc = 1;
+      yInc = 0;
+      break;
+
+    case GomokuReferee::Direction::NORTH_EAST :
+      xInc = 1;
+      yInc = 1;
+      break;
+
+    case GomokuReferee::Direction::SOUTH_EAST :
+      xInc = 1;
+      yInc = -1;
+      break;
+
+    case GomokuReferee::Direction::NORTH_WEST :
+      xInc = -1;
+      yInc = 1;
+      break;
+
+    case GomokuReferee::Direction::SOUTH_WEST :
+      xInc = -1;
+      yInc = -1;
+      break;
+
+    default :
+      xInc = 0;
+      yInc = 0;
+    }
+  if (MAP_WIDTH * (coordinates.y + (yInc * 3)) + (coordinates.x + (xInc * 3)) > MAP_SIZE)
+    return false;
+  return countCapture(map, coordinates, xInc, yInc);
 }
